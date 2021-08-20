65c6d1942ecdf978b788028dcf49eea5441a4048
vickyruud <aprajit.k@gmail.com>
2021-08-20 15:40:50 +0000

    completed the cat as service exercise

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,11 +1,19 @@
 const request = require("request");
 
-const url = `https://api.thecatapi.com/v1/breeds/search?q=siberian`;
+let breedName = process.argv[2];
+const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
 request(url, (error, status, body) => {
-  let data = JSON.parse(body);
-  console.log(data);
-  console.log(typeof data);
-})
-
-
+  if (error) {
+    console.log("Error: ", error);
+    return;
+  } else {
+    let data = JSON.parse(body);
+    if (data.length === 0) {
+      console.log("Not a valid breed!");
+      return;
+    }
+    let breed = data[0]["description"];
+    console.log(breed);
+  }
+});
 
